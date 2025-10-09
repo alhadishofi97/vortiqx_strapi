3f46b8a1bc8b9c46ad13bf315584757be92c58d9
Alhadiy Shofi <alhadinindo@gmail.com>
2025-10-09 10:57:30 +0700

    initial push

---
+++ src/api/judul/controllers/judul.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * judul controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::judul.judul');
---
+++ src/api/judul/routes/judul.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * judul router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::judul.judul');
